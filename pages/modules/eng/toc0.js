22c3282fdccbd7b5092c97f3f36ce3aa985cddf8
rk2verma <rk2verma@gmail.com>
2022-01-13 20:16:14 +0530

    final kg
    final kg

---
+++ pages/modules/eng/toc0.js
@@ -0,0 +1,30 @@
+var headTitle=" English";
+var tocDes = [{
+    "desc" : "1. Alphabet - Capital Letters ",
+    "ref" : "./eng/content/alphabet/Ch1.html?id=1"
+},
+{
+    "desc" : "2. Alphabet - Small Letters",
+    "ref" : "./eng/content/alphabet/Ch1.html?id=2"
+},
+{
+    "desc" : "3. Alphabet with phonics",
+    "ref" : "./eng/content/video/videoPlayer.html?id=0"
+},
+{
+    "desc" : "4. Number Names 1 to 20",
+    "ref" : "./eng/content/video/videoPlayer.html?id=1"
+},
+{
+    "desc" : "5. Parts of Face ",
+    "ref" : "./eng/content/pof/Part_of_face.html"
+}/*,
+{
+    "desc" : "6. Parts of Body",
+    "ref" : "./eng/content/pob/Part_of_body.html"
+},
+{
+    "desc" : "Jason",
+    "ref" : "path"
+}*/];
+createTOC(headTitle);
